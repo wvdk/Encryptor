aae790af516d5afad297f58d7a02f991af6a99a2
Wes Van der Klomp <wes.vdk@icloud.com>
2020-06-19 17:01:05 -0700

    add RNCryptor and sketch out encryption flow

--- CommandLineTool/Sources/CommandLineToolCore/CommandLineTool.swift
+++ CommandLineTool/Sources/CommandLineToolCore/CommandLineTool.swift
@@ -3,14 +3,6 @@ import Files
 
 public final class CommandLineTool {
     
-    enum Error: Swift.Error {
-        
-        case missingFileName
-        
-        case failedToCreateFile
-        
-    }
-    
     private let arguments: [String]
     
     public init(arguments: [String] = CommandLine.arguments) {
@@ -23,13 +15,31 @@ public final class CommandLineTool {
             throw Error.missingFileName
         }
         
-        let fileName = arguments[1]
- 
-        do {
-            try Folder.current.createFile(at: fileName)
-        } catch {
-            throw Error.failedToCreateFile
+        
+        guard let originalFile = try? Folder.current.file(at: arguments[1]),
+            let originalData = try? originalFile.read() else {
+            throw Error.couldNotLoadFile
         }
+        
+        let encryptedData = RNCryptor.encrypt(data: originalData, withPassword: "1234")
+        
+        let targetFileName = arguments[2]
+        
+        do {
+            try Folder.current.createFile(at: targetFileName, contents: encryptedData)
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    enum Error: Swift.Error {
+        
+        case missingFileName
+        
+        case failedToCreateFile
+        
+        case couldNotLoadFile
+        
     }
     
 }
